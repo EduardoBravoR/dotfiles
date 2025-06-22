8b4edbd2f6f53f0b9b94c55e2f7bd66f6d22f745
EduardBravoR <mentzeralexander9@gmail.com>
2025-06-22 16:58:27 -0600

    Fix .gitignore file, personalize tmux and improve caplocks detection in waybar

--- waybar/scripts/capslock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-caps=$(cat /sys/class/leds/*capslock/brightness 2>/dev/null | head -n 1)
-if [[ "$caps" == "1" ]]; then
-    echo '{"text":" A", "tooltip":"Caps Lock on"}'
-else
-    echo '{"text":"", "tooltip":"Caps Lock disabled"}'
-fi
