da52a112357c8e06198f3a92e86e76817968b2fe
EduardBravoR <mentzeralexander9@gmail.com>
2025-12-01 16:23:08 -0600

    Change kitty and nvim theme and improve tmux colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,26 @@
-return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- Load before others
-    lazy = false,    -- Load immediately at startup
-    config = function()
-        require("catppuccin").setup(
-            {
-                flavour = "mocha", -- or "mocha", "latte", "frappe", "macchiato"
-                transparent_background = false,
-            }
-        )
-        vim.cmd.colorscheme("catppuccin")
-    end
+return -- Using Lazy
+{
+  "navarasu/onedark.nvim",
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('onedark').setup {
+      style = 'dark'
+    }
+    require('onedark').load()
+  end
 }
+-- return {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     priority = 1000, -- Load before others
+--     lazy = false,    -- Load immediately at startup
+--     config = function()
+--         require("catppuccin").setup(
+--             {
+--                 flavour = "mocha", -- or "mocha", "latte", "frappe", "macchiato"
+--                 transparent_background = false,
+--             }
+--         )
+--         vim.cmd.colorscheme("catppuccin")
+--     end
+-- }
